2a49601213e01907ba4e197556f0042b78267155
james <james918310@gmail.com>
2024-05-13 22:04:55 +0800

    make change for deployment

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const courseRoute = require("./routes").course;
 const passport = require("passport");
 require("./comfig/passport")(passport);
 const cors = require("cors");
+const path = require("path");
+const port = process.env.PORT || 8080;
 
 mongoose
-  .connect("mongodb://localhost:27017/mernDB")
+  .connect(process.env.MONFODB_CONNECTION)
   .then(() => {
     console.log("connecting to mongodb...");
   })
@@ -21,6 +23,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/api/user", authRoute);
 //只有登入系統的人，才能夠去新增課程或註冊課程
@@ -31,6 +34,15 @@ app.use(
   courseRoute
 );
 
-app.listen(8080, () => {
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
   console.log("後端sever聆聽在port 8080...");
 });
